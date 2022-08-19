b92c72b40ae40206c439eff4f53758f5b2ba24c0
Iicaa <ktpica@gmail.com>
2022-08-19 13:28:56 +0700

    Masukan tugas 2

---
+++ laravel/project-2/system/routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('400', function () {
+    return view('400');
+});
+
+Route::get('account', function () {
+    return view('account');
+});
+
+Route::get('blog-archive-2', function () {
+    return view('blog-archive-2');
+});
+
+Route::get('blog-archive', function () {
+    return view('blog-archive');
+});
+
+Route::get('blog-single', function () {
+    return view('blog-single');
+});
+
+Route::get('cart', function () {
+    return view('cart');
+});
+
+Route::get('checkout', function () {
+    return view('checkout');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('product-detail', function () {
+    return view('product-detail');
+});
+
+Route::get('product', function () {
+    return view('product');
+});
+
+Route::get('wishlist', function () {
+    return view('wishlist');
+});
